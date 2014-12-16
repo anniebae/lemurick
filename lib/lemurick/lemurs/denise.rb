a87bad711aa9bfc6090f4d8204dc317070676d93
anniebae <bae.annie@gmail.com>
2014-12-16 15:24:50 -0500

    denise quotes

--- lib/lemurick/lemurs/denise.rb
+++ lib/lemurick/lemurs/denise.rb
@@ -1,4 +1,26 @@
 module Lemurick
   class Denise
+    def speak
+      quotes.sample
+    end
+
+    def quotes
+      [
+        "Guys, it's snowing",
+        "Snacks?!",
+        "Animate me lunch",
+        "I'm hungry",
+        "Ignoreignoreignore",
+        "I like my dressing with a sprinkle of lettuce",
+        "Who's ready for happy hour?",
+        "Of course I'm going! Free food & beer obvi"
+      ].map{|string| string + jamaica}
+
+
+    end
+
+    def jamaica
+      "... mon"
+    end
   end
 end
--- lib/lemurick/lemurs/matt.rb
+++ lib/lemurick/lemurs/matt.rb
@@ -1,4 +1,21 @@
 module Lemurick
   class Matt
+    def speak
+      quotes.sample
+    end
+
+    def quotes
+      [
+        "Comparison is the thief of joy",
+        "Truth is manners; manners are a fiction",
+        "Ho ho ho; ha ha ha",
+        "Wisdom in youth is an abomination",
+        "Dubious"
+      ].map{|string| string + coconut_oil}
+    end
+
+    def coconut_oil
+      "...with coconut oil"
+    end
   end
 end
