88316bf8619ff67699c216a2e49d1e487f0fb09b
Yaniv Savir <saviry@gmail.com>
2014-12-16 10:15:43 -0500

    adds yaniv model

--- lib/lemurick/lemurs/yaniv.rb
+++ lib/lemurick/lemurs/yaniv.rb
@@ -1,4 +1,17 @@
 module Lemurick
   class Yaniv
+
+    def speak
+      quotes.sample
+    end
+
+    def quotes
+      [
+        'Have you tried binding.pry yet?',
+        'Heugh?',
+        'Good morning, lemurs!'
+      ]
+    end
+
   end
 end
