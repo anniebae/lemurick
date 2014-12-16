f51bb837a1dbf585a110d5fe209e6f310fac84fa
amadden80 <amadden80@gmail.com>
2014-12-16 11:32:00 -0500

    0.0.4

--- lib/lemurick.rb
+++ lib/lemurick.rb
@@ -1,5 +1,7 @@
-require File.expand_path("../lemurick/version", __FILE__)
-require File.expand_path("../lemurick/lemurick", __FILE__)
+# require File.expand_path("../lemurick/version", __FILE__)
+# require File.expand_path("../lemurick/lemurick", __FILE__)
+require_relative "./lemurick/version"
+require_relative "./lemurick/lemurick"
 
 
 Dir['./lib/lemurick/lemurs/*'].each { |x| require File.expand_path(x) }
