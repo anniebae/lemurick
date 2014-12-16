b9a72bba76e812a2f7dd04ed9ee267b5fc2bfe56
amadden80 <amadden80@gmail.com>
2014-12-16 11:10:17 -0500

    paths use expand_path

--- lib/lemurick.rb
+++ lib/lemurick.rb
@@ -1,7 +1,8 @@
-require_relative "lemurick/version"
-require_relative "lemurick/lemurick"
+require File.expand_path("../lemurick/version", __FILE__)
+require File.expand_path("../lemurick/lemurick", __FILE__)
 
-Dir['./lib/lemurick/lemurs/*'].each { |x| require x }
+
+Dir['./lib/lemurick/lemurs/*'].each { |x| require File.expand_path(x) }
 
 module Lemurick
   # Your code goes here...
